e34543989763faae7eb1ae73570412a964028b7b
Kouichi Sumizaki <kouichi.sumizaki@gmail.com>
2014-11-24 11:48:36 +0900

    modified recursive part modified test part added enum ChopType

--- kata-2/BinaryChop.java
+++ kata-2/BinaryChop.java
@@ -1,21 +1,25 @@
 class BinaryChop{
 
+  enum ChopType {RECURSIVE, ITERATIVE, FUNCTIONAL, STD_FUNC, TREE};
+
   private int recursiveChop(int[] array, int s, int t, int n){
     int res=-1;
     if((t-s)==1){
       if(array[s]==n) res=s;
     }
     else{
-      int mid=(t-s)/2;
-      res=recursiveChop(array, s, mid, n);
-      if(res==-1) res=recursiveChop(array, mid, t, n);
+      int mid=(t+s)/2;
+      if(array[mid]>n) res=recursiveChop(array, s, mid, n);
+      else res=recursiveChop(array, mid, t, n);
     }
     return res;
   }
 
-  public int chop(int[] array, int n){
+  public int chop(int[] array, int n, ChopType T){
+  // public int chop(int[] array, int n){
     int res=-1;
-    res=recursiveChop(array, 0, array.length, n);
+    int length=array.length;
+    if(length!=0) res=recursiveChop(array, 0, array.length, n);
 
     return res;
   }
@@ -31,14 +35,31 @@ class BinaryChop{
     }
 
     try {
+      int[] case0=new int[] {};
       int[] case1=new int[] {1};
-      assert recursiveChop(case1, 0, 1, 1)==0;
-      assert chop(case1, 1)==0;
-      // assert chop(new int[] {1,2}, 2)==1;
-      // assert chop(new int[] {1,2,4}, 1)==0;
-      // assert chop(new int[] {1,2,4}, 2)==1;
-      // assert chop(new int[] {1,2,4}, 4)==2;
-      // assert chop(new int[] {1,2,4}, 0)==-1;
+      int[] case2=new int[] {1,2};
+      int[] case3=new int[] {1,2,4};
+      int[] case4=new int[] {1,2,4,9};
+      int[][] testCases={case0, case1, case2, case3, case4};
+      for(int i=0; i<testCases.length; i++){
+        int[] tmp=testCases[i];
+        for(int j=0; j<tmp.length; j++)
+          assert chop(tmp, tmp[j], ChopType.RECURSIVE)==j;
+          // assert chop(tmp, tmp[j])==j;
+        // assert chop(tmp, 0)==-1;
+        // assert chop(tmp, 3)==-1;
+        // assert chop(tmp, 10)==-1;
+      }
+      // assert chop(case0, 0)==-1;
+      // assert chop(case1, 1)==0;
+      // assert chop(case2, 2)==1;
+      // assert chop(case3, 1)==0;
+      // assert chop(case3, 2)==1;
+      // assert chop(case3, 4)==2;
+      // assert chop(case3, 0)==-1;
+      // assert chop(case3, 10)==-1;
+      // assert chop(case3, 3)==-1;
+
       System.out.println("test succeeded");
     }catch(AssertionError e){
       System.out.println("test failed");
