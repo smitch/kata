c68cc7751fdc37537fe799402158fba6247c8777
kouichi <kouichi@kiritsubo.(none)>
2015-02-15 13:20:59 +0900

    write: test code of minumumTempSpreadDay

--- kata-4/WeatherData.java
+++ kata-4/WeatherData.java
@@ -1,7 +1,24 @@
 class WeatherData{
 
   public void test(){
-    assert minimunTempSpreadDay()=14;
+    try {
+      assert 0==-1;
+      System.out.println("run with -ea option to test");
+      return;
+    }
+    catch(AssertionError e){
+      System.out.println("test start");
+    }
+
+    int ans=0, expect=0;
+    try {
+      // ans=minimunTempSpreadDay();
+      // expect=14
+      // assert ans==14;
+    }catch(AssertionError e){
+      System.out.println("test failed");
+      System.out.format("expect=%d, ans=%d\n", expect, ans);
+    }
   }
 
   public static void main(String[] args){
