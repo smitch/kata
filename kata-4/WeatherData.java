fa1d030be2b5d568e869b97a33178c433858960e
kouichi <kouichi@kiritsubo.(none)>
2015-02-15 13:04:43 +0900

    write: test code of minumumTempSpreadDay

--- kata-4/WeatherData.java
+++ kata-4/WeatherData.java
@@ -1,7 +1,10 @@
 class WeatherData{
 
+  public void test(){
+    assert minimunTempSpreadDay()=14;
+  }
+
   public static void main(String[] args){
     System.out.println("code kata: Weather Data");
   }
-
 }
