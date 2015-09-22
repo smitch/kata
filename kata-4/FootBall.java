cbb13cc44a7f84e2bea18653c8d983dd98bdc8bf
smitch <kouichi.sumizaki@gmail.com>
2015-09-22 11:57:42 +0900

    Write: test method prototype

--- kata-4/FootBall.java
+++ kata-4/FootBall.java
@@ -1,6 +1,18 @@
 class FootBall{
 
+  public void test(){
+    try{
+      assert 0==-1;
+      System.out.println("Warning: run with -ea option to test");
+      return;
+    }
+    catch(AssertionError e){
+      System.out.println("test start");
+    }
+  }
+
   public static void main(String[] args){
     System.out.println("Foot ball class!");
+    (new FootBall()).test();
   }
 }
