b207978a625b192fdecb9b9a4edbf28f84306bb8
smitch <kouichi.sumizaki@gmail.com>
2015-10-04 19:26:44 +0900

    Write: assert answer is expected value

--- kata-4/FootBall.java
+++ kata-4/FootBall.java
@@ -11,11 +11,15 @@ class FootBall{
       System.out.println("test start");
     }
 
-    int ans=0, expect=0;
+    String expect="Aston_Villa";
+    String  ans="";
 
     System.out.println("print the team with the smallest differential betweeen for and away goals");
 
     try{
+      // do process here
+
+      assert expect==ans;
 
     }catch(AssertionError e){
       System.out.println("test failed");
