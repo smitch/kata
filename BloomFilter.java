1788a0ed438a87ffff46873ca971799c2dfb3c13
smitch <kouichi.sumizaki@gmail.com>
2015-10-14 23:52:08 +0900

    write pseudo code of kada-5 sketch

--- BloomFilter.java
+++ BloomFilter.java
@@ -1,16 +1,36 @@
 class BloomFilter{
 
+  // class member
+  // bloomTable: array of bits
+  // tableSize
+
   private boolean isEnabledAssertion(){
     boolean isEnabled=false;
     assert isEnabled=true;
     return isEnabled;
   }
 
+  // private hashMethod1
+  // private hashMethod2
+  // ...
+  // private hashMethodN
+
   private void test(){
     if(!isEnabledAssertion()){
       System.out.println("Error: -ea option is required to test");
       return;
     }
+
+    // open file
+
+    // while bufferedReader != null
+    // read a word in list
+    // hash the word
+    // set the bit corresponding to the hash number on in dictionary(bloom table)
+
+    // close file
+
+    // pick a word and check the word is in the dictionary
 
     return;
   }
