c4adf088961d244b7565be3de2bdecd0d9eef781
smitch <kouichi.sumizaki@gmail.com>
2015-11-03 18:15:50 +0900

    added sources kata-7 part 1: readable code

---
+++ DivideWord1.java
@@ -0,0 +1,12 @@
+class DivideWord1{
+  // Objective: to divide 6 length word in dictionary into two words
+  //           and write code as readable as possible
+
+  // read dictionary and find 6 length word
+  // divide the word and find the partial word in dictionary
+  // if the both word are found, print the word
+
+  public static void main(String[] args){
+    System.out.println("Divide word class first design");
+  }
+}
