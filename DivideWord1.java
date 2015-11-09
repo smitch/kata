24050ecee1a1d379f6c8c199e6249bb9e6ebba61
smitch <kouichi.sumizaki@gmail.com>
2015-11-09 22:26:29 +0900

    added pseudo code of function prototype

--- DivideWord1.java
+++ DivideWord1.java
@@ -3,8 +3,15 @@ class DivideWord1{
   //           and write code as readable as possible
 
   // read dictionary and find 6 length word
-  // divide the word and find the partial word in dictionary
-  // if the both word are found, print the word
+
+  // HashSet<String> get6LengthWord
+
+  // divide the word and find substring in dictionary
+  // if the both words are found, print the word
+
+  // void findDictionary
+  // for each substring
+  // findInDictionary
 
   public static void main(String[] args){
     System.out.println("Divide word class first design");
