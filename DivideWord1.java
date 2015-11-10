a7b2350af12e6a72f9fb558fb024c95d981c8222
smitch <kouichi.sumizaki@gmail.com>
2015-11-10 19:44:03 +0900

    modified comment to more specific

--- DivideWord1.java
+++ DivideWord1.java
@@ -2,18 +2,22 @@ class DivideWord1{
   // Objective: to divide 6 length word in dictionary into two words
   //           and write code as readable as possible
 
-  // read dictionary and find 6 length word
+  // void test
 
   // HashSet<String> get6LengthWord
-
-  // divide the word and find substring in dictionary
-  // if the both words are found, print the word
+  // read file and get 6 length words
 
   // void findDictionary
   // for each substring
   // findInDictionary
 
+  // pubclic void divideWord1{
+  // read dictionary and find 6 length word
+  // divide the word and find substring in dictionary
+  // if the both words are found, print the word
+  //}
+
   public static void main(String[] args){
-    System.out.println("Divide word class first design");
+    System.out.println("Divide word class first design: readable code");
   }
 }
