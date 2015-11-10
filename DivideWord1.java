9f6f70178aed6abb4f3de1ba5fea877cb43a8866
smitch <kouichi.sumizaki@gmail.com>
2015-11-10 22:00:32 +0900

    implemented get6LengthWord method and test part

--- DivideWord1.java
+++ DivideWord1.java
@@ -5,7 +5,8 @@ class DivideWord1{
   // Objective: to divide 6 length word in dictionary into two words
   //           and write code as readable as possible
 
-  private final String wordList="wordlist.txt";
+  private final String WORD_LIST="wordlist.txt";
+  private final String WORD_LIST_6LENGTH="6lengthwordlist.txt";
 
   private boolean isAssrtionEnabled(){
     boolean isEnabled=false;
@@ -19,7 +20,8 @@ class DivideWord1{
       return;
     }
     try{
-
+      HashSet<String> wordSet=get6LengthWord();
+      assert isAll6Wordfoud(new HashSet<String>(wordSet))==true;
       System.out.println("test finished");
     }
     catch(AssertionError e){
@@ -30,12 +32,38 @@ class DivideWord1{
    return;
   }
 
-  // boolean isAll6Wordfoud
+  private boolean isAll6Wordfoud(HashSet<String> wordSet){
+    boolean ret=true;
+    try{
+      File file=new File(WORD_LIST_6LENGTH);
+      BufferedReader br=new BufferedReader(new FileReader(file));
+      String str;
+      while((str=br.readLine())!=null){
+        if(!wordSet.contains(str)){
+          System.err.println("unneccesary word is contained");
+          ret=false;
+          break;
+        }
+        wordSet.remove(str);
+      }
+      br.close();
+    }
+    catch(FileNotFoundException e){
+      System.err.println(e);
+    }
+    catch(IOException e){
+      System.err.println(e);
+    }
+    if(wordSet.isEmpty()==false)
+      ret=false;
+
+    return ret;
+  }
 
   private HashSet<String> get6LengthWord(){
     HashSet<String> set=new HashSet<String>();
     try{
-      File file=new File(wordList);
+      File file=new File(WORD_LIST);
       BufferedReader br=new BufferedReader(new FileReader(file));
       String str;
       while((str=br.readLine())!=null){
@@ -50,6 +78,7 @@ class DivideWord1{
     catch(IOException e){
       System.err.println(e);
     }
+
     return set;
   }
   // read file and get 6 length words
